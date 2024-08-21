57507acc48994a90e80da524173c9a144cbd93f5
Web-Affan-Farooq <affanamir903@gmail.com>
2024-08-21 14:20:36 +0500

    website root file

---
+++ js/index.js
@@ -0,0 +1,48 @@
+// setTimeout(() => {
+//     document.querySelector(".container").style.display = "none";
+//     let popup = document.createElement("div");
+//     popup.className = "popup";
+//     popup.innerHTML = `<p>Hello user , this website is a static template created by Muhammad Affan. If you want this template to be customized for you , reach out Muhammmad Affan via linked in, source code freely available on github. Click to copy link.
+//         Consider to star the repository </p>
+//         <p>Build with ♥ by Muhammad affan</p>
+//     <button type="button">linked in</button>
+//     <button type="button">Github</button>`;
+//     console.log(popup);
+//     document.body.appendChild(popup);
+//     popup.addEventListener('click', function copyToClipboard(e) {
+//         navigator.clipboard.writeText = "github.com";
+//     })
+//     document.body.style.display = "flex";
+//     document.body.style.justifyContent = "center";
+//     setTimeout(() => {
+//         document.body.removeChild(popup);
+//         console.log("notification removed");
+//         document.querySelector(".container").style.display = "block";
+//         document.body.style.display = "block";
+//     }, 8000);
+// }, 5000);
+
+
+
+let observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        let intersecting = entry.isIntersecting;
+        if (intersecting) {
+            entry.target.style.opacity = 1;
+        } else if (!intersecting) {
+            entry.target.style.opacity = 0;
+        }
+    })
+}, { threshold:[0.25] });
+
+let section_1 = document.querySelector(".section-1");
+let section_2 = document.querySelector(".section-2");
+let section_3 = document.querySelector(".section-3");
+let section_4 = document.querySelector(".section-4");
+let section_5 = document.querySelector(".section-5");
+let section_6 = document.querySelector(".section-6");
+let footer = document.querySelector("footer");
+let sections = [section_5, section_1, section_2, section_3, section_4, section_6, footer];
+sections.forEach(section => {
+    observer.observe(section);
+});
---
+++ js/switch.js
@@ -0,0 +1,18 @@
+let navToogle = document.querySelector("#menu-icon");
+let nav = document.querySelector(".navbar");
+
+let flag = 0;
+navToogle.addEventListener('click',function showNav() {
+    if(flag === 0) {
+        document.querySelector("header").style.height = "60%";
+        document.querySelector(".nav-list").style.display = "flex";
+        document.querySelector("#menu-icon").innerHTML = `<i class="fa-solid fa-xmark"></i>`;
+        flag = 1;
+    }
+    else {
+        document.querySelector("#menu-icon").innerHTML = `<i class="fa-solid fa-bars"></i>`;
+        document.querySelector(".nav-list").style.display = "none";
+        document.querySelector("header").style.height = "60px";
+        flag = 0;
+    }
+})
