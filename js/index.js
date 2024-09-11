515ebabf3c12aaebb9bb16c2a4dc04a95da1a85f
Web-Affan-Farooq <affanamir903@gmail.com>
2024-09-11 18:00:38 +0500

    scripts

--- js/index.js
+++ js/index.js
@@ -6,22 +6,24 @@ setTimeout(() => {
         Consider to star the repository </p>
         <p>Build with ♥ by Muhammad affan</p>
         <p>Want to exit? click on me</p>`;
-    console.log(popup);
     document.body.appendChild(popup);
     document.body.style.display = "flex";
     document.body.style.justifyContent = "center";
     document.querySelector(".popup").addEventListener('click', function exit() {
         document.body.removeChild(popup);
-        console.log("notification removed");
         document.querySelector(".container").style.display = "block";
         document.body.style.display = "block";
         window.navigator.clipboard.writeText("hello world");
     })
     setTimeout(() => {
-        document.body.removeChild(popup);
-        console.log("notification removed");
-        document.querySelector(".container").style.display = "block";
-        document.body.style.display = "block";
+        if(popup) {
+
+        }else {
+            document.body.removeChild(popup);
+            console.log("notification removed");
+            document.querySelector(".container").style.display = "block";
+            document.body.style.display = "block";
+        }
     }, 20000);
 }, 5000);
 
@@ -46,7 +48,6 @@ let section_5 = document.querySelector(".section-5");
 let section_6 = document.querySelector(".section-6");
 let footer = document.querySelector("footer");
 let sections = [ section_1, section_2, section_3, section_4, section_6, footer];
-console.log(sections);
 
 sections.forEach(section => {
     observer.observe(section);
